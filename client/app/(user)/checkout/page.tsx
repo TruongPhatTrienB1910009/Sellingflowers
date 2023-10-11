9793c84469bedf6a0938c2ac6b9a612cc6def13a
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-10-11 22:56:48 +0700

    them mot so Ui phan checkout

--- client/app/(user)/checkout/page.tsx
+++ client/app/(user)/checkout/page.tsx
@@ -1,43 +1,100 @@
 "use client"
-import { Container } from '@mui/material'
-import React from 'react'
+import { Box, Button, Container } from '@mui/material'
+import React, { useState } from 'react'
 import TableItemsCheckout from '@/components/TableItemsCheckout'
+import DialogAddress from '@/components/DialogAddress'
 import { getAllItemsInCart } from '@/services/cartService'
 
+const ComponentAddress = () => {
+    const [openDialog, setOpenDialog] = useState(false);
+
+    return (
+        <>
+            <Box sx={{
+                backgroundColor: '#fff',
+                padding: '20px',
+                marginBottom: '10px',
+                borderRadius: '5px'
+            }}>
+                <h4>Địa chỉ giao hàng</h4>
+                <div>
+
+                </div>
+            </Box>
+            <DialogAddress openDialog={openDialog} />
+        </>
+    )
+}
+
+
+const ComponentCheckout = () => {
+    const [openDialog, setOpenDialog] = useState(false);
+
+    return (
+        <>
+            <Box sx={{
+                backgroundColor: '#fff',
+                padding: '20px',
+                marginTop: '10px',
+                borderRadius: '5px'
+            }}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <div>
+                        Nhấn "Đặt hàng" đồng nghĩa với việc bạn đồng ý tuân theo Điều khoản của chúng tôi
+                    </div>
+                    <div>
+                        <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                            <span style={{fontSize: '16px', marginBottom: '6px'}}>Tổng tiền hàng: 200.000.000 đ</span>
+                            <span style={{fontSize: '16px', marginBottom: '6px'}}>Phí vận chuyển: </span>
+                            <span style={{fontSize: '16px', marginBottom: '6px'}}>Tổng tiền hàng: </span>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Button sx={{ backgroundColor: '#228b22', color: '#fff', padding: '16px 42px', ':hover': { backgroundColor: '#228b22' } }}>
+                                Đặt Hàng
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </Box>
+        </>
+    )
+}
+
+
 const CheckOut = () => {
-  const [listItemsInCart, setListItemsInCart] = React.useState<any>([]);
+    const [listItemsInCart, setListItemsInCart] = React.useState<any>([]);
 
+    const getAllProducts = async () => {
+        if (localStorage.getItem('accesstoken')) {
+            const items = await getAllItemsInCart();
+            if (items) {
+                const arrCheckOut = sessionStorage.getItem("checkout");
+                if (arrCheckOut) {
+                    const temp = (items.DT.Products.map((item: any, index: number) => {
+                        if (JSON.parse(arrCheckOut).includes(index)) {
+                            return item;
+                        }
+                    })).filter((value: any) => value != undefined);
 
-  const getAllProducts = async () => {
-    if (localStorage.getItem('accesstoken')) {
-      const items = await getAllItemsInCart();
-      if (items) {
-        const arrCheckOut = sessionStorage.getItem("checkout");
-        if (arrCheckOut) {
-          const temp = (items.DT.Products.map((item: any, index: number) => {
-            if(JSON.parse(arrCheckOut).includes(index)) {
-              return item;
+                    if (temp) {
+                        setListItemsInCart(temp);
+                    }
+                }
             }
-          })).filter((value: any) => value != undefined);
-
-          if(temp) {
-            setListItemsInCart(temp);
-          }
         }
-      }
     }
-  }
 
-  React.useEffect(() => {
-    getAllProducts();
-  }, [listItemsInCart.length]);
+    React.useEffect(() => {
+        getAllProducts();
+    }, [listItemsInCart.length]);
 
-  return (
-    <Container maxWidth='lg' sx={{marginTop: '6px'}}>
-      
-      <TableItemsCheckout listItemsInCart={listItemsInCart}/>
-    </Container>
-  )
+    return (
+        <Container maxWidth='lg' sx={{ marginTop: '6px' }}>
+            <ComponentAddress />
+            <TableItemsCheckout listItemsInCart={listItemsInCart} />
+            <ComponentCheckout />
+        </Container>
+    )
 }
 
 export default CheckOut
