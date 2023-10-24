498b0b323c581c2a12ff773859524230094cefff
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-10-24 22:14:27 +0700

    update 3

---
+++ client/components/admin/TableTypeProducts.tsx
@@ -0,0 +1,134 @@
+'use client'
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Button, ButtonGroup, Checkbox, IconButton, Tooltip } from '@mui/material';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import FindInPageIcon from '@mui/icons-material/FindInPage';
+import { VND } from '@/utils/VND';
+import '@/styles/common/tableItems.css'
+
+interface Column {
+    id: 'name' | 'description' | 'category' | 'actions';
+    label: string;
+    minWidth?: number;
+    align?: 'right' | 'left' | 'center';
+    format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+    { id: 'name', label: 'Tên Loại', minWidth: 170 },
+    {
+        id: 'category',
+        label: 'Thuộc Danh Mục',
+        minWidth: 100,
+        align: 'center',
+    },
+    {
+        id: 'description',
+        label: 'Mô Tả',
+        minWidth: 170,
+        align: 'center',
+        format: (value: number) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'actions',
+        label: 'Hành Động',
+        minWidth: 170,
+        align: 'center',
+        format: (value: number) => value.toLocaleString('en-US'),
+    }
+];
+
+
+export default function TableTypeProducts({ listtypes }: any) {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+
+
+    return (
+        (listtypes) ? (
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{ minWidth: column.minWidth }}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {listtypes
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row: any, index: number) => {
+                                    return (
+                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                            <TableCell>
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell>
+                                                category
+                                            </TableCell>
+                                            <TableCell>
+                                                {row.description}
+                                            </TableCell>
+                                            <TableCell align='center'>
+                                                <Tooltip title="Xem Chi Tiết" placement="top">
+                                                    <IconButton>
+                                                        <FindInPageIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Chỉnh Sửa" placement="top">
+                                                    <IconButton>
+                                                        <BorderColorIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Xóa" placement="top">
+                                                    <IconButton>
+                                                        <DeleteIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={listtypes.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        ) : ''
+    );
+}
